e80f15c5daceef08d9049aaf69d72370886dbe3f
kendulee41 <kendulee41@gmail.com>
2022-03-18 21:16:41 +0700

    nop bai tap ngay 18/03

---
+++ module1/ss7_bien_kieu_du_lieu_toan_tu/practice/BaiThucHanh2.js
@@ -0,0 +1,8 @@
+let width;
+let height;
+width = prompt("Enter the width");
+height = prompt("Enter the height");
+width = parseInt(width);
+height = parseInt(height);
+let area = width * height;
+document.write('The area is: ' + area);
